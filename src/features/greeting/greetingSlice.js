c1b760345cb01a5e9d7e37367a72b8bf5dd5ec7f
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-09 13:50:20 +0100

    Updated setup

---
+++ src/features/greeting/greetingSlice.js
@@ -0,0 +1,28 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchMessage = createAsyncThunk(
+  "message/fetchMessage",
+  async () => {
+    const response = await axios.get("/api/v1/greeting_random");
+    return response.data;
+  }
+);
+
+export const greetingSlice = createSlice({
+  name: "message",
+  initialState: { message: "", status: "idle" },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMessage.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchMessage.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.message = action.payload.content;
+      });
+  },
+});
+
+export default greetingSlice.reducer;
