88e44a3d4221377b35d10cd1853164f1204ae1fb
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-09 22:46:53 +0000

    Update index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import App from './App';
+import './index.css';
 
 const router = createBrowserRouter([
   {
